25b9f8c610cdd577e3cfe04b37683c8205f17b8e
Soar <85624529@qq.com>
2015-09-07 18:46:31 +0800

    add other import parg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,7 +22,42 @@ class HomeController < ApplicationController
      province_sheet = province_book.worksheet 0
      station_sheet = station_book.worksheet 0
      points_sheet = points_book.worksheet 0
-      
+
+     province_sheet.each_with_index do |row,index |
+       if index >= 1
+         description = row[1].strip
+         province = Province.where(description: description).try(:first)
+         if province.nil?
+            Province.create description: description
+         end
+       end
+     end
+
+     province_sheet.each_with_index do |row,index |
+       if index >= 1
+         province_desc = row[1].strip
+         city_desc = row[2].strip
+         province = Province.where(description: province_desc).first
+         city = City.where(description: city_desc).try(:first)
+         if city.nil?
+           City.create! description: city_desc , province: province
+         end
+       end
+     end
+     
+     province_sheet.each_with_index do |row,index |
+       if index >= 1
+         province_desc = row[1].strip
+         city_desc = row[2].strip
+         station_desc = row[3].strip
+         city = City.where(description: city_desc).try(:first)
+         station = Station.where(description: station_desc).try(:first)
+         if station.nil?
+           Station.create! description: station_desc, stationable: city
+         end
+       end
+     end
+
      points_sheet.each_with_index do |row,index |
        if index >= 1
          station_desc = row[1].strip
