132277e8517422bf80ec8f1212ba715068a25765
Soar <85624529@qq.com>
2015-09-18 17:23:54 +0800

    doing move the assset

--- app/assets/javascripts/map-global.js
+++ app/assets/javascripts/map-global.js
@@ -1,1 +1,8 @@
 window.HOST_TYPE = 1;
+var assets =  assets || {};
+
+var protocol =  window.location.protocol
+var serverHost = window.location.host;
+assets.blankGifPath = protocol +"//" + serverHost + "/assets/blank.gif"
+
+console.log("blnk:"+ assets.blankGifPath);
