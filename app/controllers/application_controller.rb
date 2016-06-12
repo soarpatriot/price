7e41fad605722ebb36a61925dd35013b5d6d2766
Soar <85624529@qq.com>
2016-06-12 11:45:23 +0800

    [feature] add rights api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend:true
-
+  
+  before_action :authorized!
   before_action :authenticate_login! 
   after_action  :allow_iframe
 
@@ -21,7 +22,7 @@ class ApplicationController < ActionController::Base
         end
    
       end
-   end
+    end
      
     def authenticate_login!
       # cookies[:LoginUserInfo] = "aaa"
@@ -29,7 +30,40 @@ class ApplicationController < ActionController::Base
         authenticate_user!
       end
     end
+    def authorized!
+      cookie_value = !cookies[:LoginUserInfo] 
+      origin_url = request.original_url
+      logger.info "origin_url: #{origin_url}"
+      if Rails.env.production?
+        if cookie_value.nil?
+          logger.info "cookie_value is nil"
+          not_allowed 
+          return 
+        end
+        code = re_pms origin_url, cookie_value
+        logger.info "code: #{code}"
+        unless code == "200"
+          not_allowed 
+        end
+      end
+    end
+
+    def re_pms origin_url, cookie_value
+        right_url = "#{Settings.pms_base_url}/right/url?url=#{origin_url}&cookieValue=#{cookie_value}"
+        response = RestClient.get right_url
+        result_hash = JSON.parse response,:symbolize_names => true
+        result_hash[:code]
+ 
+    end
+   
     def allow_iframe 
       response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://login.wuliusys.com"
     end
+
+    def not_allowed
+      respond_to do |format|
+        format.html { render file: "#{Rails.root}/public/422", layout: false, status: :not_allowed }
+      end
+    end
+
 end
