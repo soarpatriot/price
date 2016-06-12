b4f360adb993d0e9d254edbfbbe08fa093f19b2c
Soar <85624529@qq.com>
2016-06-12 23:14:00 +0800

    [optimize] change the error page

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Price
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.exceptions_app = self.routes
+    #config.exceptions_app = self.routes
     config.i18n.default_locale = :"zh-CN"
     config.i18n.fallbacks = [:en]
     config.i18n.available_locales = [:en, :"zh-CN"]
