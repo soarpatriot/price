36025ad0a2ecb665de92b572c27b4e07c33152d0
Soar <85624529@qq.com>
2016-08-25 11:27:01 +0800

    [optimize] optimze the result show

--- app/assets/javascripts/station-expressmen.js
+++ app/assets/javascripts/station-expressmen.js
@@ -1,6 +1,6 @@
 
 $(function(){
-  if( $("#stations-expressmen").length > 0 || $("#stations-selected_man").length > 0){
+  if( $("#stations-expressmen").length > 0 || $("#stations-selected_man").length > 0 || $("#stations-syn_all").length > 0 ){
     $("#man_ids").select2({ width: '200px' ,language:"zh-CN" ,theme:"bootstrap"}); 
   }
 });
