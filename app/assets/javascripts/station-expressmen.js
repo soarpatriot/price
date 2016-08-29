314409144ac959eb81c0392901d38493abe42e39
Soar <85624529@qq.com>
2016-08-29 15:16:56 +0800

    [optimize] opt the delete man logic

--- app/assets/javascripts/station-expressmen.js
+++ app/assets/javascripts/station-expressmen.js
@@ -1,6 +1,7 @@
 
 $(function(){
-  if( $("#stations-expressmen").length > 0 || $("#stations-selected_man").length > 0 || $("#stations-syn_all").length > 0 ){
+  if( $("#stations-expressmen").length > 0 || $("#stations-selected_man").length > 0 || $("#stations-syn_all").length > 0 
+    || $("#stations-man").length > 0 ){
     $("#man_ids").select2({ width: '200px' ,language:"zh-CN" ,theme:"bootstrap"}); 
   }
 });
