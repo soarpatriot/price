032cb0417ae0553c893217c140bd0f87be91656b
Soar <85624529@qq.com>
2016-08-29 15:41:12 +0800

    [optimize] remove iframe check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,8 +62,9 @@ class ApplicationController < ActionController::Base
     end
    
     def allow_iframe 
-      response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM #{Settings.parent_frame}"
-      response.headers["Content-Security-Policy"] = "frame-ancestors  #{Settings.parent_frame}"
+      response.headers.except! 'X-Frame-Options'
+      #response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM #{Settings.parent_frame}"
+      #response.headers["Content-Security-Policy"] = "frame-ancestors  #{Settings.parent_frame}"
       #response.headers["X-FRAME-OPTIONS"] = "GOFORIT"
     end
 
